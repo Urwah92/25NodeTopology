43a9d2376000f527f92e8639d34b350e9ad1a064
Urwah92 <76911824+Urwah92@users.noreply.github.com>
2026-04-16 15:32:20 +0200

    Update service discovery script to use new version

--- 25node_topo/serfapp/config_service_discovery.sh
+++ 25node_topo/serfapp/config_service_discovery.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-python3 service_discovery_v7.py \
-         --geom-url http://172.20.20.17:4040/cluster-status \
-        --rtt-threshold-ms 12 \
-        --rpc-addr 127.0.0.1:7373 --timeout-s 8 \
-        --sort score_per_cpu --limit 30 \
-        --http-serve --http-host 0.0.0.0 --http-port 4041 --http-path /hilbert-output \
-        --buyer-url http://127.0.0.1:8090/buyer \
-        --loop --busy-secs 50
+./service_discovery_v9_multidim_full_final_sh \
+      --geom-url http://172.20.20.17:4040/cluster-status \
+      --rtt-threshold-ms 12 \
+      --rpc-addr 127.0.0.1:7373 --timeout-s 8 \
+      --pct-start 0.25 --max-steps 8 \
+      --sort score_per_cpu --limit 30 \
+      --http-serve --http-host 0.0.0.0 --http-port 4041 --http-path /hilbert-output \
+      --post-output-url http://localhost:5665/initiate_tx \
+      --buyer-url http://127.0.0.1:8090/buyer
+
